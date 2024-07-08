8d7f26ca5b8e2584d18bcd433d55ef554d2724bb
user17040 <118284299+user17040@users.noreply.github.com>
2024-07-08 18:38:43 +0800

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', function () {
     const idioms = data1.split(' ').concat(Object.keys(data2));
     const solution = generateSolution();
@@ -40,68 +41,72 @@ document.addEventListener('DOMContentLoaded', function () {
     function evaluateGuess(guess) {
         const guessRow = document.createElement('div');
         guessRow.className = 'guess';
-
+    
         // 跟踪已经被标记过的位置
         const solutionMarked = Array(4).fill(false);
         const cornersMarked = Array(4).fill(null).map(() => Array(5).fill(false));
-
+    
         // 创建格子并标记四角号码
         guess.split('').forEach((char, index) => {
             const box = document.createElement('div');
             box.className = 'box';
             box.textContent = char;
-
+    
             const corners = get_4corners(char);
             const solutionCorners = solution.split('').map(c => get_4corners(c));
-
+    
             for (let i = 0; i < corners.length; i++) {
                 let className = '';
-
+    
                 // 标记完全正确的四角号码
                 if (corners[i] === solutionCorners[index][i] && !cornersMarked[index][i]) {
                     className = 'green';
                     cornersMarked[index][i] = true;
                 }
-
+    
                 const positions = ['top-left', 'top-right', 'bottom-left', 'bottom-right', 'right-middle'];
                 box.innerHTML += `<div class="corner ${positions[i]} ${className}">${corners[i]}</div>`;
             }
-
+    
             if (char === solution[index] && !solutionMarked[index]) {
                 box.classList.add('green');
                 solutionMarked[index] = true;
             }
-
+    
             guessRow.appendChild(box);
         });
-
+    
         // 再次遍历，用于标记部分正确的字符和四角号码
         guess.split('').forEach((char, index) => {
             const box = guessRow.children[index];
             const corners = get_4corners(char);
             const solutionCorners = solution.split('').map(c => get_4corners(c));
-
+    
             for (let i = 0; i < corners.length; i++) {
-                for (let j = 0; j < solutionCorners.length; j++) {
-                    if (index !== j && corners[i] === solutionCorners[j][i] && !cornersMarked[j][i]) {
-                        box.children[i].classList.add('yellow');
-                        cornersMarked[j][i] = true;
-                        break;
+                if (!cornersMarked[index][i]) {
+                    for (let j = 0; j < solutionCorners.length; j++) {
+                        if (index !== j && corners[i] === solutionCorners[j][i] && !cornersMarked[j][i]) {
+                            box.children[i].classList.add('yellow');
+                            cornersMarked[j][i] = true;
+                            break;
+                        }
                     }
                 }
             }
-
+    
             for (let i = 0; i < solution.length; i++) {
-                for (let j = 0; j < solution.length; j++) {
-                    if (index !== j && char === solution[j] && !solutionMarked[j][i]) {
-                        box.classList.add('yellow');
-                        solutionMarked[j][i] = true;
-                        break;
+                if (!solutionMarked[index][i]) {
+                    for (let j = 0; j < solution.length; j++) {
+                        if (index !== j && char === solution[j] && !solutionMarked[j][i]) {
+                            box.classList.add('yellow');
+                            solutionMarked[j][i] = true;
+                            break;
+                        }
                     }
                 }
             }
-
-
+    
+    
         });
 
         guesses.appendChild(guessRow);
